2f2197753b5a1da8635affdf2881f81e2f2fe90c
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-10-23 08:38:56 -0700

    Agregar archivos de proyecto.

---
+++ PracticaConsultorio/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticaConsultorio
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardarNuevoPaciente_Click(object sender, RoutedEventArgs e)
+        {
+            Paciente nuevoPaciente = new Paciente();
+            nuevoPaciente.Nombre = txtNombre.Text;
+            nuevoPaciente.Dirrecion = txtDirrecion.Text;
+            nuevoPaciente.Edad = int.Parse(txtEdad.Text);
+            nuevoPaciente.altura = float.Parse(txtAltura.Text);
+            nuevoPaciente.EnfermedadesCronicas = txtEnfermedadesCronicas.Text;
+            Datos.pacientes.Add(nuevoPaciente);
+            actualizarListaPacientes();
+
+            txtNombre.Text = string.Empty;
+            txtDirrecion.Text = string.Empty;
+            txtEdad.Text = string.Empty;
+            txtPeso.Text = string.Empty;
+            txtAltura.Text = string.Empty;
+            txtEnfermedadesCronicas.Text = string.Empty;
+            gridNuevoPaciente.Visibility = Visibility.Collapsed;
+        }
+        private void actualizarListaPacientes()
+        {
+            lstPacientes.Items.Clear();
+            foreach(Paciente paciente in Datos.pacientes)
+            {
+                lstPacientes.Items.Add(new ListBoxItem()
+                {
+                    Content = paciente.Nombre
+                }
+                );
+            }
+        }
+
+        private void btnNuevoPaciente_Click(object sender, RoutedEventArgs e)
+        {
+            gridNuevoPaciente.Visibility = Visibility.Visible;
+            btnGuardarNuevoPaciente.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ PracticaConsultorio/Paciente.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaConsultorio
+{
+    class Paciente
+    {
+        public string Nombre { get; set; }
+        public string Dirrecion { get; set; }
+        public string Telefono { get; set; }
+        public int Edad { get; set; }
+        public float Altura { get; set; }
+        public string EnfermedadesCronicas { get; set; }
+
+        public Paciente()
+        {
+            Nombre = "Nuevo Paciente";
+            Dirrecion = "Nueva Dirrecion";
+            Telefono = "Nuevo Telefono";
+            Edad = 00;
+            Altura = 00;
+            EnfermedadesCronicas = "Nueva Enfermedad";
+        }
+        public Paciente(string nombre, string dirrecion, string telefono, int edad, float altura, string enfermedadescronicas)
+        {
+            Nombre = nombre;
+            Dirrecion = dirrecion;
+            Telefono = telefono;
+            Edad = edad;
+            Altura = altura;
+            EnfermedadesCronicas = enfermedadescronicas;
+        }
+    }
+}
